29ab40646706d1b95dbb88591ac40598dd0a6720
schaehun <schaehun@student.42.fr>
2024-06-28 01:07:02 +0200

    [FEATURE] product search, frontend connect to database

--- srcs/frontend/app/(home)/page.tsx
+++ srcs/frontend/app/(home)/page.tsx
@@ -1,11 +1,53 @@
 import { Metadata } from "next";
 import { SearchInput } from "../../components/searchInput";
+import { ProductCard } from "../../components/productCard";
+import { resourceLimits } from "worker_threads";
 
 export const metadata: Metadata = {
 	title: "Home",
 };
 
-export default function HomePage() {
+// const TEST_PRODUCT_DATA = {
+// 	img: "productcard_image.png",
+// 	productTitle: "버리긴 아깝고 쓸데없는 물건",
+// 	productPrice: "28.5",
+// 	userNickname: "82sien",
+// 	meetLocation: "75004 Paris",
+// };
+
+const URL = "http://golang:8080/graphql";
+
+async function getProducts() {
+	const response = await fetch(URL, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			query: `{
+				item(ItemId: \"Item1\") {
+					ItemId
+					UserId
+					Title
+					Description
+					Price
+					Images
+					Location
+					CreatedAt
+				}
+			}`,
+		}),
+	});
+	if (response.ok) {
+		const json = await response.json();
+		return json;
+	}
+	return [];
+}
+
+export default async function HomePage() {
+	const productsJSON = await getProducts();
+	// const products = JSON.stringify(productsJSON.data.item);
 	return (
 		<div>
 			<div>
@@ -18,10 +60,14 @@ export default function HomePage() {
 				<p><strong>최근</strong>에 올라온거 뭐<strong>있냥</strong>?</p>
 				<div>
 					<ul>
-						<li>item 1</li>
-						<li>item 2</li>
-						<li>item 3</li>
-						<li>item 4</li>
+						<ProductCard 
+							userid={productsJSON.data.item.UserId}
+							title={productsJSON.data.item.Title}
+							description={productsJSON.data.item.Description}
+							price={productsJSON.data.item.Price}
+							images={productsJSON.data.item.Images}
+							location={productsJSON.data.item.Location}
+						/>
 					</ul>
 				</div>
 			</div>
@@ -37,10 +83,10 @@ export default function HomePage() {
 				</div>
 				<div>
 					<ul>
-						<li>item 1</li>
+						{/* <li>item 1</li>
 						<li>item 2</li>
 						<li>item 3</li>
-						<li>item 4</li>
+						<li>item 4</li> */}
 					</ul>
 				</div>
 			</div>
--- srcs/frontend/app/search/[id]/page.tsx
+++ srcs/frontend/app/search/[id]/page.tsx
@@ -4,11 +4,29 @@ export const metadata: Metadata = {
 	title: "Result",
 };
 
-export default function SearchResultPage({params: {id}}: {params: {id: string}; }) {
+async function getSearchResult(id: string) {
+	return await fetch('http://golang:8080/graphql', {
+		method: 'POST',
+		headers: {
+
+		},
+		body: JSON.stringify({
+			query: `{
+				query { itemSearch(Title: ${id}) } {
+					id
+				}
+			}`
+		})
+	}).then(response => response.json());
+}
+
+export default async function SearchResultPage({params: {id}}: {params: {id: string}; }) {
+	const result = await getSearchResult(id);
 	return (
 		<div>
 			<p>상세페이지</p>
 			<p>"{id}" 검색 결과</p>
+			<p>{result.id}</p>
 		</div>
 	);
 }
