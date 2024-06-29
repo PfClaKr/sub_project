6761f5e90313c9746550358e15973d07032ea7b8
YugeonCHUN <PfClaKr@github.com>
2024-06-29 19:53:47 +0200

    [ADD] elasticsearch configuration automated in apiserver

--- srcs/apiserver/elasticsearchHandler.go
+++ srcs/apiserver/elasticsearchHandler.go
@@ -5,6 +5,10 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"log"
+	"strings"
+	"time"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/elastic/go-elasticsearch/v8"
@@ -20,7 +24,109 @@ func initElasticsearch() {
 			"http://elasticsearch:9200",
 		},
 	}
-	es, _ = elasticsearch.NewClient(cfg)
+	var err error
+	es, err = elasticsearch.NewClient(cfg)
+	if err != nil {
+		log.Fatalf("Error creating the Elasticsearch client: %s", err)
+	}
+
+	// Elasticsearch가 준비될 때까지 재시도
+	for {
+		res, err := es.Info()
+		if err != nil {
+			log.Printf("Error getting Elasticsearch info: %s. Retrying...", err)
+			time.Sleep(5 * time.Second)
+			continue
+		}
+		if res.IsError() {
+			log.Printf("Elasticsearch not ready: %s. Retrying...", res.String())
+			time.Sleep(5 * time.Second)
+			continue
+		}
+		break
+	}
+
+	settings := `{
+		"settings": {
+			"index": {
+				"analysis": {
+					"tokenizer": {
+						"my_nori_tokenizer": {
+							"type": "nori_tokenizer",
+							"decompound_mode": "mixed",
+							"discard_punctuation": "false"
+						},
+						"my_ngram_tokenizer": {
+							"type": "ngram",
+							"min_gram": 2,
+							"max_gram": 3
+						}
+					},
+					"filter": {
+						"stopwords": {
+							"type": "stop",
+							"stopwords": " "
+						}
+					},
+					"analyzer": {
+						"my_nori_analyzer": {
+							"type": "custom",
+							"tokenizer": "my_nori_tokenizer",
+							"filter": ["lowercase", "stop", "trim", "stopwords", "nori_part_of_speech"],
+							"char_filter": ["html_strip"]
+						},
+						"my_ngram_analyzer": {
+							"type": "custom",
+							"tokenizer": "my_ngram_tokenizer",
+							"filter": ["lowercase", "stop", "trim", "stopwords", "nori_part_of_speech"],
+							"char_filter": ["html_strip"]
+						}
+					}
+				}
+			}
+		},
+		"mappings": {
+			"properties": {
+				"ProductName": {
+					"type": "text",
+					"analyzer": "standard",
+					"search_analyzer": "standard",
+					"fields": {
+						"nori": {
+							"type": "text",
+							"analyzer": "my_nori_analyzer",
+							"search_analyzer": "my_nori_analyzer"
+						},
+						"ngram": {
+							"type": "text",
+							"analyzer": "my_ngram_analyzer",
+							"search_analyzer": "my_ngram_analyzer"
+						}
+					}
+				}
+			}
+		}
+	}`
+
+	ctx := context.Background()
+	exists, err := es.Indices.Exists([]string{"nori_sample"})
+	if err != nil {
+		log.Fatalf("Error checking if index exists: %s", err)
+	}
+	if exists.StatusCode == 404 {
+		req := esapi.IndicesCreateRequest{
+			Index: "nori_sample",
+			Body:  strings.NewReader(settings),
+		}
+		res, err := req.Do(ctx, es)
+		if err != nil {
+			log.Fatalf("Error creating index: %s", err)
+		}
+		defer res.Body.Close()
+		if res.IsError() {
+			log.Fatalf("Error response from Elasticsearch: %s", res.String())
+		}
+	}
 }
 
 func addItemToElasticsearch(item map[string]*dynamodb.AttributeValue) error {
